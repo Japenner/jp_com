9b58f7f9ef016d23faad09bb0b0abe07fb01b3bb
Jacob Penner <jacob.penner@daveramsey.com>
2022-09-14 14:35:51 -0400

    add basic github action deployment config file

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header/Header';
-import ToDoListContainer from './components/ToDo/ToDoListContainer';
-
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <ToDoListContainer></ToDoListContainer>
+      <div>
+        <h1>Coming Soon!</h1>
+      </div>
     </div>
   );
 }
