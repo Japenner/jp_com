168ac6dc5057558e03e95ac45e6c6deeeac5ed38
Jacob Penner <japenner@gmail.com>
2023-04-07 09:22:09 -0400

    feat: add primereact and primeflex with proof of concept for linktree clone (#26)
    * yarn add primereact primeicons
    
    * update documentation and configs
    
    * reference primereact styling
    
    * add primeflex and react-transition-group
    
    * first pass as linktree poc

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+
+// Styling imports
 import './App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+import { Avatar } from 'primereact/avatar';
+import { Button } from 'primereact/button'
 
 function App() {
+  const colors: any = {
+    youtube: 'red',
+    twitter: 'blue',
+    github: 'black',
+  }
+
+  const data = {
+    name: 'Jacob Penner',
+    image: 'https://github.com/japenner.png',
+    bio: "Hi there, my name is Jacob Penner. I'm a full stack developer at Ad Hoc",
+    links: [
+      {
+        name: 'My GitHub',
+        url: 'https://github.com/japenner',
+        icon: 'github',
+      },
+      {
+        name: 'My LinkedIn',
+        url: 'https://www.linkedin.com/in/japenner/',
+        icon: 'linkedin',
+      },
+    ]
+  }
+
   return (
-    <div className="App">
-      <div>
-        <h1>Coming Soon!</h1>
+    <div className='m-4'>
+      <div className="flex justify-content-center align-items-center">
+        <Avatar
+          image={data.image}
+          size="xlarge"
+          shape="circle"
+        />
+        <h1 className="m-2">{data.name}</h1>
+      </div>
+      <div className='flex justify-content-center align-items-center'>
+        <p>{data.bio}</p>
+      </div>
+      <div className='flex justify-content-center align-items-center'>
+        <div className='flex flex-column'>
+          {data.links.map((link) => (
+            <Button
+              className="m-2 p-button-outlined"
+              style={{ color: colors[link.icon] }}
+            >
+              <i className={`pi pi-${link.icon} px-3`} ></i>
+              {link.name}
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
