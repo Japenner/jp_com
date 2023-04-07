ca4c1b264cd16c3a5b1a8d6cec47ee9cf198c5fe
Jacob Penner <japenner@gmail.com>
2023-04-07 14:21:58 -0400

    feat: add new pages, navigation, and animations to navigating

---
+++ react_ui/src/components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnimatedPage from '../AnimatedPage';
+
+const Contact = () => {
+  return (
+    <AnimatedPage>
+      Contact coming soon!
+    </AnimatedPage>
+  )
+};
+
+export default Contact;
