1ac5e4df09857dd10fe0a0c6dee812a2be0f9e98
Jacob Penner <japenner@gmail.com>
2023-04-07 07:06:48 -0400

    reference primereact styling

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
