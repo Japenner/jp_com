a160581577b6fb7394c73807bacf284388c0c6fb
Jacob Penner <japenner@gmail.com>
2023-04-19 14:41:41 -0400

    update entrypoint script for rails api to create/migrate/seed db

--- .docker/rails_api/entrypoint.sh
+++ .docker/rails_api/entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /rails_api/tmp/pids/server.pid
 
+rails db:prepare
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
