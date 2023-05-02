a8d760928baa0537001eb844442c05c14131b748
Jacob Penner <japenner@gmail.com>
2023-05-02 09:44:27 -0400

    add section styling to css index

--- react_ui/src/index.tsx
+++ react_ui/src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./scenes/global/DotNav/DotNav.css";
 import "./scenes/global/Topbar/Topbar.css";
-import "./scenes/pages/Landing/Landing.css";
-import "./scenes/pages/Home/HomePage.css";
 import "./scenes/pages/Notes/NotesPage.css";
+import "./scenes/sections/About/About.css";
+import "./scenes/sections/Contact/Contact.css";
+import "./scenes/sections/Landing/Landing.css";
+import "./scenes/sections/Resume/Resume.css";
+import "./App.css"
 import App from "./App";
 
 const root = ReactDOM.createRoot(
