5bb6cc5d51c93f95a1e50d81a991eb661ed6a462
Jacob Penner <japenner@gmail.com>
2023-11-07 15:01:07 -0500

    add build directory back in, building will need to happen locally for now (#46)

---
+++ react_ui/build/env-config.js
@@ -0,0 +1,9 @@
+window._env_ = {
+  COMPOSE_PROJECT_NAME: "jp_com",
+  POSTGRES_USER: "postgres",
+  POSTGRES_PASSWORD: "postgres",
+  RAILS_ENV: "development",
+  REACT_APP_API_URL: "http://localhost:3001/api/v1",
+  REACT_APP_RAILS_API_PORT: "3001",
+  REACT_APP_REACT_UI_PORT: "4000",
+}
